922ade703d80704953990caa6cf9de4d03f2cf31
Owner <Owner@Owner-PC.attlocal.net>
2017-01-30 19:59:38 -0600

    V 1.1.2  Base Program. Lab1 File corrected.

--- src/lab1/Employee.java
+++ src/lab1/Employee.java
@@ -12,16 +12,102 @@ import java.util.Date;
  * @version     1.02
  */
 public class Employee {
-    String firstName;
-    String lastName;
-    public String ssn;
-    boolean metWithHr;
-    boolean metDeptStaff;
-    boolean reviewedDeptPolicies;
-    boolean movedIn;
-    String cubeId;
-    Date orientationDate;
+    
+    // Variables reformated for ecnapsilation
+    // All properties set to private
+    private String firstName;  
+    private String lastName;   
+    private String ssn;        
+    private String cubeID;     
+    private boolean metWithHr;
+    private boolean metDeptStaff;
+    private boolean reviewedDeptPolicies;
+    private boolean movedIn;
+    private Date orientationDate;
 
 
+    // Constructor to create mandatory employee inforation
+    public Employee (String firstName, String lastName, String ssn) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ssn = ssn;
+    }
+    
+    
+    // Getters
+    
+    public String getFirstName () {
+        return firstName;
+    }
+    
+    public String getLastName () {
+        return lastName;
+    }
+    
+    public String getSsn () {
+        return ssn;
+    }
+    
+    public String getCubeID () {
+        return cubeID;
+    }
+    
+    public boolean getMetWithHr () {
+        return metWithHr;
+    }
+    
+    public boolean getMetDeptStaff () {
+        return metDeptStaff;
+    }
+    
+    public boolean getReviewedDeptPolicies () {
+        return reviewedDeptPolicies;
+    }
+    
+    public boolean getMovedIn () {
+        return movedIn;
+    }
+    
+    public Date getOrientationDate () {
+        return orientationDate;
+    }
+
+    // Setters
+    
+    public void setFirstName (String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public void setLastName (String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public void setSsn (String ssn) {
+        this.ssn = ssn;
+    }
+    
+    public void setCubeID (String cubeID) {
+        this.cubeID = cubeID;
+    }
+    
+    public void setMetWithHr (boolean metWithHr) {
+        this.metWithHr = metWithHr;
+    }
+    
+    public void setMetDeptStaff (boolean metDeptStaff) {
+        this.metDeptStaff = metDeptStaff;
+    }
+    
+    public void setReviewedDeptPolicies (boolean reviewedDeptPolicies) {
+        this.reviewedDeptPolicies = reviewedDeptPolicies;
+    }
+    
+    public void setMovedIn (boolean movedIn) {
+        this.movedIn = movedIn;
+    }
+    
+    public void setOrientationDate (Date orientationDate) {
+        this.orientationDate = orientationDate;
+    }
 
 }
